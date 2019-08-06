678206e241da7883096aac0c0505fc6f00857066
Alexander Riccio <test35965+learn@gmail.com>
2019-08-06 16:36:57 -0400

    solve lab yo

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,22 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {value: ""};
+  }
+
+  handleTyping(event) {
+    // console.log(event.target);
+    this.setState({value: event.target.value})
+    // this.props.value = event.target.value;
+    // console.log(this.state);
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event => this.handleTyping(event)} value={this.state.value} />
+        {this.props.maxChars - this.state.value.length}
       </div>
     );
   }
